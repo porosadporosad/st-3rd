8c1cb1d5ab99bd39be8140684c659660ef4b5e58
porosadporosad <tmxk1594@naver.com>
2024-01-26 20:07:13 +0900

    ex1

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,21 @@ function App() {
   ];
 
   // TODO: filter를 사용하여 18세 이상의 학생들만 선택하세요.
-  const filteredStudents = students.filter(/* 여기에 코드 작성 */);
+  const filteredStudents = students.filter((item) => {
+    return item.age >= 18;
+  });
+
+  const clickName = (item) => {
+    alert(`${item.age}, ${item.grade}`);
+  };
 
   return (
     <div>
       <h1>학생 목록</h1>
       <ul>
-        {/* TODO: map을 사용해서 filteredStudents를 여기에 렌더링하세요. */}
+        {filteredStudents.map((item) => {
+          return <p onClick={() => clickName(item)}>{item.name}</p>;
+        })}
         {/* TODO: 학생이름을 클릭하면 나이와 점수가 alert 돼야 해요.*/}
       </ul>
     </div>
